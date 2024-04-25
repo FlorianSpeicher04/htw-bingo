0bd4288ef73015cb5dc4da674c4869468c2367ba
FlorianSpeicher04 <flo200538@gmail.com>
2024-04-25 21:32:30 +0200

    init commit

---
+++ src/test/java/de/htwsaar/bingo/BingoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwsaar.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
